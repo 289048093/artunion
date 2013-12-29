4294a2897ec7e734ba0e4733e115b343b8ea05e3
289048093 <geniuslizhao@gmail.com>
2013-12-29 17:36:28 +0800

    modify

---
+++ code/trunk/src/main/java/com/artunion/entity/StudentEntity.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.artunion.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * @author Lizhao
+ *
+ */
+@Entity
+@Table(name="tb_student")
+public class StudentEntity extends UserEntity {
+
+}
---
+++ code/trunk/src/main/java/com/artunion/entity/TeacherEntity.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.artunion.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * @author LiZhao
+ *
+ */
+@Entity
+@Table(name="tb_student")
+public class TeacherEntity extends UserEntity {
+    
+}
